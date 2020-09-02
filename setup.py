c94a21fc7ca5157c1c5420a4bbf49107674e7d83
Nathan J. Mehl <n@oden.io>
2020-09-02 14:55:56 -0400

    loosen six dependency

--- setup.py
+++ setup.py
@@ -1,34 +1,33 @@
 import setuptools
 from distutils.core import setup
 
-__VERSION__ = '1.3.2'
+__VERSION__ = "1.3.3"
 
 setup(
-    name='pydpkg',
-    packages=['pydpkg'],  # this must be the same as the name above
+    name="pydpkg",
+    packages=["pydpkg"],  # this must be the same as the name above
     version=__VERSION__,
-    description='A python library for parsing debian package control headers and comparing version strings',
-    author='Nathan J. Mehl',
-    author_email='n@climate.com',
-    url='https://github.com/theclimatecorporation/python-dpkg',
-    download_url='https://github.com/theclimatecorporation/python-dpkg/tarball/%s' % __VERSION__,
-    keywords=['apt', 'debian', 'dpkg', 'packaging'],
-    setup_requires=[
-        'wheel'
-    ],
+    description=(
+        "A python library for parsing debian package control headers and comparing"
+        " version strings"
+    ),
+    author="Nathan J. Mehl",
+    author_email="pypi@memory.blank.org",
+    url="https://github.com/memory/python-dpkg",
+    download_url="https://github.com/memory/python-dpkg/tarball/%s" % __VERSION__,
+    keywords=["apt", "debian", "dpkg", "packaging"],
+    setup_requires=["wheel"],
     install_requires=[
-        'arpy==1.1.1',
-        'backports.lzma==0.0.14',
-        'six==1.10.0',
-        'PGPy==0.4.1'
+        "arpy==1.1.1",
+        "backports.lzma==0.0.14",
+        "six<2.0.0",
+        "PGPy==0.4.1",
     ],
     extras_require={
-        'test': ['pep8==1.7.0', 'pytest==3.1.1', 'pylint==2.3.1'],
-        'publish': ['twine']
+        "test": ["pep8==1.7.0", "pytest==3.1.1", "pylint==2.3.1"],
+        "publish": ["twine"],
     },
-    scripts=[
-        'scripts/dpkg-inspect.py'
-    ],
+    scripts=["scripts/dpkg-inspect.py"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: Apache Software License",
@@ -38,5 +37,5 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: System :: Archiving :: Packaging",
-        ]
+    ],
 )
